44ee0e82b4406c92d0a55f93787781bdef2fea2e
rcooper3307 <rcooper3307@bths.edu>
2018-12-21 08:33:00 -0500

    okay the code works now

--- src/com/company/TokenPass.java
+++ src/com/company/TokenPass.java
@@ -24,12 +24,15 @@ public class TokenPass {
 
     public void distributeCurrentPlayerTokens()
     {
-        int distribute = board[currentPlayer];
-        board[currentPlayer] = 0;
-        for(int i = 1; distribute > 0; i++)
-        {
-            //how do i get it to loop back to the first number?
-            board[currentPlayer + i] = board[currentPlayer + i] + 1;
+        int playerTokens = board[currentPlayer]; //number of tokens
+        int Player = currentPlayer + 1; //start distributing tokens to this player
+        board[currentPlayer] = 0; //take away the tokens from currentPlayer
+        for(int i = playerTokens; i > 0; i--) { //starts with the number of tokens, runs until you run out //of tokens
+            if(Player >= board.length) { //if c is out of the board go to 0
+                Player = 0;
+            }
+            board[Player] = board[Player] + 1;
+            Player++; //go to next player
         }
     }
 }
